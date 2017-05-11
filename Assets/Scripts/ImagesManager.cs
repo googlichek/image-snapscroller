06a668438aa753943a798dff958ec2b3e34923f1
Goga Akobiya <googlichek.one@gmail.com>
2017-05-11 15:26:53 +0300

    snapscroller: Assuring that project is in work condition and uploading to github.

---
+++ Assets/Scripts/ImagesManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImagesManager : MonoBehaviour
+{
+	public Image ImagePrefab;
+
+	private string _path;
+
+	private void Awake()
+	{
+		_path = Application.streamingAssetsPath;
+		InitiateImageCreation();
+	}
+
+	private void InitiateImageCreation()
+	{
+		string[] fileNames = Directory.GetFiles(_path, "*.png").ToArray();
+
+		for (int i = 0; i < fileNames.Length; i++)
+		{
+			StartCoroutine(CreateImage(fileNames[i], i));
+		}
+	}
+
+	private IEnumerator CreateImage(string url, int index)
+	{
+		WWW www = new WWW("file://" + url);
+		yield return www;
+
+		Image image = Instantiate(ImagePrefab, transform);
+
+		Texture texture = www.texture;
+		Sprite loadedSprite =
+			Sprite.Create(
+				(Texture2D) texture,
+				new Rect(0, 0, texture.width, texture.height),
+				Vector2.zero);
+
+		Vector2 currentPosition = new Vector2(1920*index, 0);
+
+		image.sprite = loadedSprite;
+		image.transform.localPosition = currentPosition;
+	}
+}
---
+++ Assets/Scripts/ScrollRectSnap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSnap : MonoBehaviour
+{
+	public RectTransform Panel;
+	public Image[] ImageContainer;
+	public RectTransform Center;
+
+	private float[] _distanceToTheCenters;
+	private bool _dragging = false;
+	private int _distanceBetweenImages;
+	private int _currentImage;
+
+	private void Update()
+	{
+		Prepare();
+
+		for (int i = 0; i < ImageContainer.Length; i++)
+		{
+			_distanceToTheCenters[i] =
+				Mathf.Abs(Center.transform.position.x - ImageContainer[i].transform.position.x);
+		}
+
+		float minDistance = Mathf.Min(_distanceToTheCenters);
+
+		for (int i = 0; i < ImageContainer.Length; i++)
+		{
+			if (Math.Abs(minDistance - _distanceToTheCenters[i]) < 1)
+			{
+				_currentImage = i;
+			}
+		}
+
+		if (!_dragging)
+		{
+			LerpToImage(_currentImage*-_distanceBetweenImages);
+		}
+	}
+
+	public void StartDrag()
+	{
+		_dragging = true;
+	}
+
+	public void EndDrag()
+	{
+		_dragging = false;
+	}
+
+	private void Prepare()
+	{
+		int containerLenght = Panel.transform.childCount;
+		ImageContainer = new Image[containerLenght];
+
+		for (int i = 0; i < containerLenght; i++)
+		{
+			ImageContainer[i] = Panel.transform.GetChild(i).GetComponent<Image>();
+		}
+
+		_distanceToTheCenters = new float[containerLenght];
+
+		if (ImageContainer[0] && ImageContainer[1])
+		{
+			_distanceBetweenImages =
+				(int)
+					Mathf.Abs(ImageContainer[1].GetComponent<RectTransform>().anchoredPosition.x -
+					          ImageContainer[0].GetComponent<RectTransform>().anchoredPosition.x);
+		}
+	}
+
+	private void LerpToImage(int position)
+	{
+		float newX = Mathf.Lerp(Panel.anchoredPosition.x, position, Time.deltaTime*5f);
+		Vector2 newPosition = new Vector2(newX, Panel.anchoredPosition.y);
+
+		Panel.anchoredPosition = newPosition;
+	}
+}
